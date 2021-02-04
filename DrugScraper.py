4b5c163bf19e6f142cc0cf10b63a215a33e08e40
sxivman <saba.inasaridze.1@btu.edu.ge>
2021-02-04 23:56:41 +0400

    made much smaller

--- DrugScraper.py
+++ DrugScraper.py
@@ -1,16 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+import PySimpleGUI as window
 
-def Drug_scraper():
-    global GeneralInfo, DetailedInfo
-    DrugName = str(input('ჩაწერეთ წამლის სახელი ქართულად: '))
-    SearchPage = requests.get(f'http://www.vidal.ge/search?query={DrugName}')
-    SearchPageContent = BeautifulSoup(SearchPage.text, 'html.parser')
+DrugName = str(input('ჩაწერეთ წამლის სახელი ქართულად: '))
+print('გთხოვთ მოიცადოთ, მიმდინარეობს წამლის ძიება...')
+
+def Drug_Scraper(Name):
+
+    #Firstly we search for drug on vidals search page and then we convert that to content
+    SearchPage = requests.get(f'http://www.vidal.ge/search?query={Name}')
+    SearchPageContent = BeautifulSoup(SearchPage.content, 'html.parser')
+
+    #Then we search for Drug URL, go to that link and convert that into content
     PageCaption = SearchPageContent.find('figcaption', class_ = 'col-xs-10').h6.a
     DrugLink = PageCaption.get('href')
     DrugPage = requests.get(DrugLink)
-    DrugPageContent = BeautifulSoup(DrugPage.text, 'html.parser')
+    DrugPageContent = BeautifulSoup(DrugPage.content, 'html.parser')
+
+    #Then we scrape text from that page
     GeneralInfo = DrugPageContent.find('div', class_ = 'col-xs-12 col-sm-7 p_x_0').get_text()
     DetailedInfo = DrugPageContent.find('figcaption', class_ = 'item-info').get_text()
+    return GeneralInfo, DetailedInfo
 
-Drug_scraper()
