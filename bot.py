a0101f5b93e546b005cb0438fd62727b78acda7e
sxivman <saba.inasaridze.1@btu.edu.ge>
2021-02-05 18:20:06 +0400

    made some changes

--- bot.py
+++ bot.py
@@ -1,14 +1,24 @@
-import requests
+import discord
+from discord import client
+from discord.errors import ClientException
+from discord.ext import commands
+import random
 from bs4 import BeautifulSoup
-import PySimpleGUI as window
+import requests
+import textwrap
 
-DrugName = str(input('ჩაწერეთ წამლის სახელი ქართულად: '))
-print('გთხოვთ მოიცადოთ, მიმდინარეობს წამლის ძიება...')
+client = commands.Bot(command_prefix = '+')
 
-def Drug_Scraper(Name):
+@client.event
+async def on_ready():
+    print('bot ready')
+
+
+@client.command(aliases = ['წამალი'])
+async def DrugInfo(ctx, Drugname):
 
     #Firstly we search for drug on vidals search page and then we convert that to content
-    SearchPage = requests.get(f'http://www.vidal.ge/search?query={Name}')
+    SearchPage = requests.get(f'http://www.vidal.ge/search?query={Drugname}')
     SearchPageContent = BeautifulSoup(SearchPage.content, 'html.parser')
 
     #Then we search for Drug URL, go to that link and convert that into content
@@ -20,9 +30,9 @@ def Drug_Scraper(Name):
     #Then we scrape text from that page
     GeneralInfo = DrugPageContent.find('div', class_ = 'col-xs-12 col-sm-7 p_x_0').get_text()
     DetailedInfo = DrugPageContent.find('figcaption', class_ = 'item-info').get_text()
-    #return GeneralInfo, DetailedInfo
-    lenght = len(DetailedInfo)
-    sss = round(6500 / 2000 + 0.5)
-    print(lenght)
+    
+    await ctx.send(textwrap.wrap(DetailedInfo, 2000))
 
-Drug_Scraper(DrugName)
+
+
+client.run('ODA2OTUxNjYwOTgyNjMyNDQ5.YBw6TA._mk7TbfMKc6vjzV4vCuMVcL9ahs')
